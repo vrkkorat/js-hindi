a0a36e9f8754bd6c195e4a133d9fda27d2889575
vrkkorat <162120157+vrkkorat@users.noreply.github.com>
2025-07-18 04:48:10 +0000

    add new files for practice

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "vivek@gmail.com"
+var accountPass = "123"
+accountcity = "junagadh"
+
+// accountId = 2 not allowed
+accountEmail = "vivek"
+accountPass = "1232312312"
+accountcity = "jjjjj"
+let accountState;
+console.log(accountId);
+
+/*
+prefer not use to var datatype
+because of issue in block scope and functional scope
+*/
+
+// console.table([accountId,accountEmail,accountcity,accountPass,accountState]);
+
+console.log(3+3+ "3" + 3);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using node js not browser 
+
+console.log(3+3)
+console.log("vivek")
